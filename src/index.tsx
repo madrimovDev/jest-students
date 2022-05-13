c98268530a87f819715eb2fbf2725076b57c8142
madrimov <m.@gm.co>
2022-05-13 14:56:14 +0500

    added commets #App

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
-import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
-import "./assets/index.css"
+// * Material UI
 import { CssBaseline } from "@mui/material"
-import App from "./App"
 import { ThemeContextProvider } from "./context/ThemeContext"
+// * assets
+import "./assets/index.css"
+// * components
+import App from "./App"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
